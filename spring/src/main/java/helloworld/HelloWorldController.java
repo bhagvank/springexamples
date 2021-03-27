7498984916330fec71a76db890c68d3f046b8a1d
bhagvank <bhagvan.kommadi@apples-MacBook-Air.local>
2021-03-28 00:53:04 +0530

    added spring

---
+++ spring/src/main/java/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package helloworld;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World\n";
+    }
+    
+}
